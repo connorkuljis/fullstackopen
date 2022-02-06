8cfe6c6a27dedfdf55932a55a344b9a33c38b4b7
connorkuljis <conkuljis@gmail.com>
2022-02-07 03:09:59 +0800

    finished part 1

--- part1/src/App.js
+++ part1/src/App.js
@@ -16,10 +16,10 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming wihout an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-  const points = Array(anecdotes.length).fill(0)
-  const copy = [...points]
-
   const [selected, setSelected] = useState(0)
+  const [mostVotesIndex, setMostVotesIndex] = useState(0)
+  const [points, setPoints] = useState([0,0,0,0,0,0,0])
+
 
   const handleSelectedClick = () => {
       const min = 0
@@ -30,17 +30,31 @@ const App = () => {
   }
 
   const handleVote = () => {
-      points[selected] += 1
-      console.log(points)
+      const copy = [...points] 	// basically useState objects are immutable
+      copy[selected] += 1 	// have to read them into a variable. edit contents of that variable, and set it to the new array
+      setPoints(copy)
+      console.log(copy)
+      // cool
+      //
+      // calc the most votes index
+      const max = Math.max(...copy)
+      const i   = copy.indexOf(max)
+      setMostVotesIndex(i)
   }
-
 
   return (
     <div>
-      {anecdotes[selected]}
       <div>
+        <h1>Anecdote of the Day</h1>
+	{anecdotes[selected]}
+        <p>has {points[selected]} votes</p>
         <Button handler={handleVote} text="vote" />
         <Button handler={handleSelectedClick} text="next anecdote" />
+      </div>
+      <div>
+        <h1>Anecdote with the most votes </h1>
+        <div>{anecdotes[mostVotesIndex]}</div>
+        
       </div>
     </div>
   )
