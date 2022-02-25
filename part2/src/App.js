d2320ac1864c6456dce8462cceff962beb16d022
connorkuljis <conkuljis@gmail.com>
2022-02-25 17:18:43 +0800

    ex 2.6 - 2.9

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,56 +1,78 @@
-import React from 'react'
-import Course from './components/Course'
+import { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
-    const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  const personsToShow = newFilter.length > 0
+    ? persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    : persons
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log(event.target.value)
+    const newPerson = { 
+      name: newName, 
+      number: newNumber
     }
-  ]
+    if (persons.filter(s => s.name === newPerson.name).length > 0) {
+      alert(newPerson.name + " is already added to phonebook")
+    }
+    else {
+      setPersons(persons.concat(newPerson))
+    }
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+    console.log(personsToShow)
+  }
 
   return (
     <div>
-	<Course course={courses[0]} />
-	<Course course={courses[1]} />
+      <h2>Phonebook</h2>
+      <div>
+	filter shown with
+	<input value={newFilter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: 
+	    <input value={newName} onChange={handleNameChange}/>
+        </div>
+	<div>
+	  number:
+	    <input value={newNumber} onChange={handleNumberChange}/>
+	</div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+	  {personsToShow.map(person => 
+	    <Person key={person.name} person={person} /> 
+	  )}
+      </div>
     </div>
   )
 }
